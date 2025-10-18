f6edaed55952c9666a7d19360a25c8582fcd309b
SWTDeveloper <sproutwingsdeveloper@gmail.com>
2025-10-18 15:03:02 +0530

    Integrate react-leaflet map into Dashboard page
    Added react-leaflet and @types/leaflet dependencies. Updated Dashboard.tsx to display a Leaflet map with a marker and popup. Included Leaflet and Leaflet Routing Machine CSS in global styles for proper map rendering.

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,6 +1,13 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 import usertracker from "../features/mqtt";
+import {
+  MapContainer,
+  Marker,
+  Polyline,
+  Popup,
+  TileLayer,
+} from "react-leaflet";
 
 const Dashboard = () => {
   const { user } = useSelector((state: any) => state.auth);
@@ -8,9 +15,25 @@ const Dashboard = () => {
     usertracker(user);
   }, []);
 
+  const center = [51.505, -0.09];
+  const rectangle = [
+    [51.49, -0.08],
+    [51.5, -0.06],
+  ];
+
   return (
     <div className="w-full h-full flex justify-center items-center text-[var(--text)]">
-      <h1>Dashboard</h1>
+      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 };
