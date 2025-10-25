872da201eb6ecc412c91d3905157dc6d7054de23
SWTDeveloper <sproutwingsdeveloper@gmail.com>
2025-10-25 18:50:39 +0530

    Add Redux slice for live vehicle status data
    Introduces a new Redux slice to manage live vehicle status, including actions to set and update the vehicle status in the store. This will support real-time updates of vehicle data in the application.

---
+++ src/store/live_data_slice.ts
@@ -0,0 +1,48 @@
+import {
+  configureStore,
+  createSlice,
+  type PayloadAction,
+} from "@reduxjs/toolkit";
+
+interface initialState {
+  vehicleStatus: {
+    user: string;
+    timestamp: number;
+    time: string;
+    lat: number;
+    lng: number;
+    speed: number;
+  };
+}
+
+const initialState = {
+  vehicleStatus: {
+    user: "",
+    timestamp: 0,
+    time: "",
+    lat: 0,
+    lng: 0,
+    speed: 0,
+  },
+};
+
+const vehicleStatusDashboard = createSlice({
+  name: "live",
+  initialState,
+  reducers: {
+    setVehicleStatus: (state, action: PayloadAction<any>) => {
+      state.vehicleStatus = action.payload;
+    },
+    updateVehicleStatus: (state, action: PayloadAction<any>) => {
+      let data = action.payload;
+      let vehicleAvailability = state.vehicleStatus?.user === data.user;
+      if (vehicleAvailability) {
+        state.vehicleStatus = data;
+      }
+    },
+  },
+});
+
+export default vehicleStatusDashboard.reducer;
+export const { setVehicleStatus, updateVehicleStatus } =
+  vehicleStatusDashboard.actions;
