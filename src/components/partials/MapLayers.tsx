2e0b706c38236af779e1fffca9906aa74b2a883e
SWTDeveloper <sproutwingsdeveloper@gmail.com>
2025-10-25 17:48:08 +0530

    Add onClick handler to map layers image
    Added an onClick event to the map layers image to trigger handleLayers with the 'street' argument. This enables users to switch to the street layer by clicking the image.

--- src/components/partials/MapLayers.tsx
+++ src/components/partials/MapLayers.tsx
@@ -6,11 +6,13 @@ import type { FC } from "react";
 
 const MapLayers: FC<maplayersProps> = (props) => {
   const { handleLayers } = props;
+
   const { theme } = useSelector((state: any) => state.auth);
   //const map = useMap();
   return (
     <div className="border-1 border-[var(--border)] bg-[var(--button)] absolute top-1 right-1 z-[999] rounded-md">
       <img
+        onClick={() => handleLayers("street")}
         src={theme === "dark" ? layersDark : layers}
         alt=""
         className="w-10"
